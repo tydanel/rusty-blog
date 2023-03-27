831f4c475695cc71155d388c89ac91db4336d8bc
Tyrell Daniel Nelson <ty.dan.nel@gmail.com>
2023-03-27 09:35:56 +0200

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,31 @@ mod blog_post;
 mod error;
 
 use actix_web;
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
-use actix_web::middleware::{Logger};
 use surrealdb::engine::remote::ws::{Client, Ws};
 use surrealdb::opt::auth::Root;
 use surrealdb::Surreal;
 
 use env_logger::Env;
 
-
 static DB: Surreal<Client> = Surreal::init();
 
 #[actix_web::main] // or #[tokio::main]
-async fn main() -> Result<(), Box< dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    
+
     DB.connect::<Ws>("localhost:8000").await?;
 
     DB.signin(Root {
         username: "root",
         password: "root",
-    }).await?;
-    
+    })
+    .await?;
 
     DB.use_ns("namespace").use_db("database").await?;
-    
-    println!("Database connected");
 
+    println!("Database connected");
 
     HttpServer::new(|| {
         App::new()
@@ -44,4 +42,31 @@ async fn main() -> Result<(), Box< dyn std::error::Error>> {
     .await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{
+        http::{header::ContentType},
+        test
+    };
+
+
+    #[actix_web::test]
+    async fn test_index_not_found() {
+        let app = test::init_service(
+            App::new()
+                .service(blog_post::list)
+        ).await;
+        
+        let req = test::TestRequest::default()
+            .insert_header(ContentType::plaintext())
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        
+        assert!(resp.status().is_client_error());
+
+    }
 }
